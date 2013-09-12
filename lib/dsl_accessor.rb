1dadac059fa3fd6b9d6ac4f7c41a3d378e67b2c5
maiha <maiha@wota.jp>
2013-09-13 01:23:19 +0900

    [1.9] disable warning about blankslate

--- lib/dsl_accessor.rb
+++ lib/dsl_accessor.rb
@@ -2,6 +2,14 @@ unless Module.new.respond_to?(:delegate)
   require File.dirname(__FILE__) + "/../core_ext/module/delegation"
 end
 
+module DslAccessor
+  if RUBY_VERSION >= "1.9"
+    BlankSlate = ::BasicObject
+  else
+    require 'blankslate'
+  end
+end
+
 require File.dirname(__FILE__) + '/dsl_accessor/version'
 require File.dirname(__FILE__) + '/dsl_accessor/auto_declare'
 require File.dirname(__FILE__) + '/dsl_accessor/accessor'
